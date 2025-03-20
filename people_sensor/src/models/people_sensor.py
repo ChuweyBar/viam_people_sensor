07f8a0c5abb8add2df52a4c0bc83f25d1eef66cd
chuwei.guo <chuwei0807@gmail.com>
2025-03-20 17:37:59 -0400

    basic feature complete

--- people_sensor/src/models/people_sensor.py
+++ people_sensor/src/models/people_sensor.py
@@ -8,16 +8,25 @@ from viam.proto.common import Geometry, ResourceName
 from viam.resource.base import ResourceBase
 from viam.resource.easy_resource import EasyResource
 from viam.resource.types import Model, ModelFamily
-from viam.utils import SensorReading, ValueTypes
+from viam.utils import SensorReading, ValueTypes, struct_to_dict
+
+from viam.logging import getLogger
 
 from viam.services.vision import Vision, VisionClient
+
+from viam.services.vision import Detection
+
+LOGGER = getLogger(__name__)
 
 class PeopleSensor(Sensor, EasyResource):
     # To enable debug-level logging, either run viam-server with the --debug option,
     # or configure your resource/machine to display debug logs.
     MODEL: ClassVar[Model] = Model(ModelFamily("cwb", "people_sensor"), "people_sensor")
 
-    _dependencies: Mapping[ResourceName, ResourceBase]
+    _configFields: Dict
+    _label: str
+    _visionClient: VisionClient
+    _cameraName: str
 
     @classmethod
     def new(
@@ -46,6 +55,16 @@ class PeopleSensor(Sensor, EasyResource):
         Returns:
             Sequence[str]: A list of implicit dependencies
         """
+        fields = struct_to_dict(config.attributes)
+        label = fields.get('label', None)
+        if label is None:
+            raise Exception("Config must specify 'label' field for sensor to detect.")
+
+        cameraName = fields.get('camera_name', None)
+        if cameraName is None:
+            raise Exception("Config must specify 'camera_name' for sensor to detect.")
+
+        # need to have dependency on vision service
         return []
 
     def reconfigure(
@@ -57,9 +76,25 @@ class PeopleSensor(Sensor, EasyResource):
             config (ComponentConfig): The new configuration
             dependencies (Mapping[ResourceName, ResourceBase]): Any dependencies (both implicit and explicit)
         """
-        self._dependencies = dependencies
-        self.logger.info("self._dependencies %r", self._dependencies)
+        self._configFields = struct_to_dict(config.attributes)
+
+        # given validate_config this should be safe
+        self._label = self._configFields['label']
+        self.logger.debug('Detecting label %r', self._label)
+
+        self._cameraName = self._configFields['camera_name']
+        self.logger.debug('Detecting via camera %r', self._cameraName)
+
+        visionClients = list(dependencies.values())
+        # hardcoding, should easily support multiple visionClients
+        if len(visionClients) > 1:
+            raise Exception("Currently does not support more than 1 dependency.")
+        if len(visionClients) < 1:
+            raise Exception("For sensor to function properly, please add a vision/mlmodel service as dependency.")
+        self._visionClient = visionClients[0]
+        LOGGER.info('self._visionClient %r', self._visionClient)
         return super().reconfigure(config, dependencies)
+
 
     async def get_readings(
         self,
@@ -73,8 +108,18 @@ class PeopleSensor(Sensor, EasyResource):
         peopleDetector = Vision.get_resource_name(visionServiceName)
         assert isinstance(peopleDetector, Vision)
         """
-        self.logger.error("`get_readings` is not implemented")
-        raise NotImplementedError()
+
+        detections = await self._visionClient.get_detections_from_camera(camera_name=self._cameraName)
+        labelString = self._label.lower() + "_detected"
+        for detection in detections:
+            if detection.class_name == self._label:
+                return {
+                    labelString: 1
+                }
+        return {
+            labelString: 0
+        }
+        
 
     async def do_command(
         self,
